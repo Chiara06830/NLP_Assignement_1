1b6e41a9eaaba54908425030786b4523f50b8c0a
Chiara <chiara.relevat@outlook.fr>
2022-11-10 18:32:05 +0100

    vrai matrice de confusion

--- English_detector.py
+++ English_detector.py
@@ -1,29 +1,35 @@
-from nltk import *
 from nltk.corpus import *
 from nltk.corpus.reader.util import *
+from nltk.metrics import ConfusionMatrix
 import nltk.data
-import nltk
-import random
-from math import *
 nltk.download('udhr2')
 nltk.download('ppattach')
+nltk.download('movie_reviews')
+import random
+from math import floor
 
 # ----------------------- TRAIN A CLASSIFIER ----------------------- 
-# open two corpus (Universal Declaration of Human Right)
+# open corpus
 short_en = open(nltk.data.find('corpora/udhr2/eng.txt')).read() # english
+short_en2 = open(nltk.data.find('corpora/movie_reviews/pos/cv878_15694.txt')).read() # english
 short_fr = open(nltk.data.find('corpora/udhr2/fra.txt')).read() # non-english
+short_tyv = open(nltk.data.find('corpora/udhr2/tyv.txt')).read() # non-english
 
 # prepare list of examples and corresponding labels
 documents = []
 for r in short_en.split('\n'):
     documents.append((r, "en"))
+for r in short_en2.split('\n'):
+    documents.append((r, "en"))
 for r in short_fr.split('\n'):
+    documents.append((r, "non_en"))
+for r in short_tyv.split('\n'):
     documents.append((r, "non_en"))
 random.shuffle(documents)
 
 # decide what feature is relevant, here: the whole word
 def english_features(word) :
-    return {'word': word}
+    return {'word': word.lower().lstrip()}
 
 # set of features based on corpus
 featuresets = [(english_features(n), lang) for (n, lang) in documents]
@@ -34,36 +40,11 @@ testing_set =  featuresets[:set_length]
 # create classifier
 classifier = nltk.NaiveBayesClassifier.train(training_set)
 
-# ----------------------- TEST THE CLASSIFIER ----------------------- 
-tp = 0 # True positive
-fp = 0 # False positive
-fn = 0 # False negative
-tn = 0 # True negative
-
-# Accumulate data for confusion matrix
-def confusion_matrix(sys_res, gold_res):
-    global tp
-    global fp
-    global fn
-    global tn
-    if sys_res == gold_res:
-        if sys_res: tp +=1
-        else: tn += 1
-    else:
-        if gold_res: fn += 1
-        else: fp += 1
-
-confusion_matrix(classifier.classify(english_features("Ceci est une phrase en Francais")), False)
-confusion_matrix(classifier.classify(english_features("This is an English sentence")), True)
-
-# Test all brown
-for id in brown.fileids():
-    confusion_matrix(classifier.classify(english_features(" ".join(brown.words(id)))), True)
-
-# test all uhdr
-for id in udhr2.fileids():
-    confusion_matrix(classifier.classify(english_features(" ".join(udhr2.words(id)))), False)
-
-print("Precision : ", (tp/(tp+fp)))
-print("Recall : ", (tp/tp+fn))
-print("Accuracy : ", ((tp+tn)/(tp+fp+tn+fn)))
+# ----------------------- TEST THE CLASSIFIER -----------------------
+gold_set = [[token[1] for token in gold_actu] for gold_actu in[nltk.pos_tag(nltk.word_tokenize(sentence[0])) for sentence in documents[set_length:]]]
+gold = []; [[gold.append(token) for token in elem]for elem in gold_set]
+test_set = [[token[1] for token in test_actu]for test_actu in[nltk.pos_tag(nltk.word_tokenize(sentence[0])) for sentence in documents[:set_length]]]
+test = []; [[test.append(token) for token in elem]for elem in test_set]
+cm = nltk.ConfusionMatrix(gold[:3000], test[:3000])
+print(cm.evaluate())
+print("Accuracy: ", nltk.classify.accuracy(classifier, testing_set))
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Assignement 1
+## Instruction
 The assignment consists in the development, in NLTK a Naïve Bayes Classifier able to detect a single class in one of the corpora available as attachments to the chosen package, by distinguishing ENGLISH against NON-ENGLISH. In particular the classifier has to be:
 
 1. Trained on a split subset of the chosen corpus, by either using an existing partition between sample documents for training and for test or by using a random splitter among the available ones;
@@ -7,3 +8,55 @@ The assignment consists in the development, in NLTK a Naïve Bayes Classifier ab
 The test of the classifier shall give out the measures of **accuracy**, **precision**, **recall** on the obtained confusion matrix and WILL NOT BE EVALUATED ON THE LEVEL OF THE PERFORMANCES. In other terms, when the confusion is produced, then the value of the assignment will be good, independently of the percentage of false positive and negative results.
 
 Deliver a short set of comments on the experience (do not deliver the entire code, but link it on a public repository like GitHub or the GATE Repo). Discuss: size of the corpus, size of the split training and test sets, performance indicators employed and their nature, employability of the classifier as a Probabilistic Language Model.
+
+## Results
+This programme is trained on four corpus. There is has much english as non-english, unified in same array with tag `en` or `non-en`.  
+Each sentences in this array is transformed in lower case and with no space at beginning and end of it.
+This array is divided in two set of equal size: the training set and the testing set.  
+Then the classifier is trained on training set and accuracy precision and recall on testing set.
+
+A possible output is:
+```
+Tag | Prec.  | Recall | F-measure
+-----+--------+--------+-----------
+   ( | 0.0000 | 0.0000 | 0.0000
+   ) | 0.0000 | 0.0000 | 0.0000
+   , | 0.0435 | 0.0630 | 0.0514
+   . | 0.0337 | 0.0306 | 0.0321
+   : | 0.0000 | 0.0000 | 0.0000
+  CC | 0.0345 | 0.0353 | 0.0349
+  CD | 0.0185 | 0.0213 | 0.0198
+  DT | 0.0513 | 0.0584 | 0.0546
+  FW | 0.0330 | 0.0395 | 0.0359
+  IN | 0.0829 | 0.0838 | 0.0833
+  JJ | 0.0898 | 0.0735 | 0.0808
+ JJR | 0.0000 | 0.0000 | 0.0000
+ JJS | 0.0000 | 0.0000 | 0.0000
+  MD | 0.0000 | 0.0000 | 0.0000
+  NN | 0.1921 | 0.1915 | 0.1918
+ NNP | 0.1500 | 0.1372 | 0.1433
+NNPS | 0.0000 | 0.0000 | 0.0000
+ NNS | 0.0338 | 0.0347 | 0.0342
+ PDT | 0.0000 | 0.0000 | 0.0000
+ POS | 0.0000 | 0.0000 | 0.0000
+ PRP | 0.0000 | 0.0000 | 0.0000
+PRP$ | 0.0000 | 0.0000 | 0.0000
+  RB | 0.0000 | 0.0000 | 0.0000
+ RBR | 0.0000 | 0.0000 | 0.0000
+ RBS | 0.0000 | 0.0000 | 0.0000
+  RP | 0.0000 | 0.0000 | 0.0000
+  TO | 0.0182 | 0.0250 | 0.0211
+  VB | 0.0000 | 0.0000 | 0.0000
+ VBD | 0.0000 | 0.0000 | 0.0000
+ VBG | 0.0000 | 0.0000 | 0.0000
+ VBN | 0.0000 | 0.0000 | 0.0000
+ VBP | 0.0000 | 0.0000 | 0.0000
+ VBZ | 0.0149 | 0.0143 | 0.0146
+ WDT | 0.0000 | 0.0000 | 0.0000
+  WP | 0.0000 | 0.0000 | 0.0000
+ WP$ | 0.0000 | 0.0000 | 0.0000
+ WRB | 0.0000 | 0.0000 | 0.0000
+  `` | 0.1667 | 0.1250 | 0.1429
+
+Accuracy:  0.6144578313253012
+```
